00553ce38299a9b49424fb34a069b0f695658a74
Shamimul Alam <bd.shawon.edu@gmail.com>
2019-05-11 12:01:56 +0600

    Query bulider

--- app/Blog.php
+++ app/Blog.php
@@ -6,8 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    public function blog()
-    {
-        return ['title'=>'this is blog title','details'=>'This is blog details'];
-    }
+
 }
--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Blog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BlogController extends Controller
 {
     public function index()
     {
+        $blog = \DB::table('blog');
+        $blog = $blog->where('title','like','%title%');
+        $blog = $blog->get();
+
+        dd($blog);
         $blog = new Blog();
         $data['blog'] = $blog->blog();
         $data['page_title'] = 'This is blog page';
