3e23aef68f58bacaaecf96a353735acf3aede760
Shamimul Alam <bd.shawon.edu@gmail.com>
2019-05-17 10:50:50 +0600

    Migration Done

---
+++ database/migrations/2019_05_17_044538_modify_blogs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->text('file');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            //
+        });
+    }
+}
