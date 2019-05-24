c15a64ac50e1ea45b5c6ee3c5dbb9195aa53682f
Shamimul Alam <bd.shawon.edu@gmail.com>
2019-05-24 11:30:31 +0600

    Master Template Implemented

--- resources/views/blog/edit.blade.php
+++ resources/views/blog/edit.blade.php
@@ -1,29 +1,15 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>{{ $title }}</title>
-</head>
-<body>
-<div class="row">
-    <div class="col-md-6">
-        <h1>{{ $title }}</h1>
+@extends('layout.master')
+@section('content')
+    <h1>{{ $title }}</h1>
 
-        <form method="post" action="{{ route('blog.update',$blog->id) }}">
-            @csrf
-            <input type="hidden" name="_method" value="PUT">
-            <input required value="{{ $blog->title }}" class="form-control" type="text" name="title" placeholder="Blog Title">
-            <br>
-            <textarea required class="form-control" name="details" id="" cols="30" rows="10">
+    <form method="post" action="{{ route('blog.update',$blog->id) }}">
+        @csrf
+        <input type="hidden" name="_method" value="PUT">
+        <input required value="{{ $blog->title }}" class="form-control" type="text" name="title" placeholder="Blog Title">
+        <br>
+        <textarea required class="form-control" name="details" id="" cols="30" rows="10">
                 {{ $blog->details }}
             </textarea>
-            <button class="btn btn-info" type="submit">Update</button>
-        </form>
-    </div>
-</div>
-</body>
-</html>
+        <button class="btn btn-info" type="submit">Update</button>
+    </form>
+@endsection
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,43 +1,28 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>{{ $title }}</title>
-</head>
-<body>
-<div class="row">
-    <div class="col-md-6">
-        <a class="btn btn-info" href="<?php echo route('blog.create') ?>">Add new blog</a>
-        <table class="table table-bordered">
+@extends('layout.master')
+@section('content')
+    <a class="btn btn-info" href="<?php echo route('blog.create') ?>">Add new blog</a>
+    <table class="table table-bordered">
+        <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Details</th>
+            <th>Actions</th>
+        </tr>
+        <!--            --><?php //foreach ($blogs as $blog){ ?>
+        @foreach($blogs as $blog)
             <tr>
-                <th>Id</th>
-                <th>Title</th>
-                <th>Details</th>
-                <th>Actions</th>
+                <td>{{ $blog->id }}</td>
+                <td>{{ $blog->title }}</td>
+                <td>{{ $blog->details }}</td>
+                <td>
+                    <a class="btn btn-info" href="{{ route('blog.edit',$blog->id) }}">Edit</a>
+                    <form action="{{ route('blog.destroy',$blog->id) }}" method="post">
+                        @csrf
+                        <input type="hidden" name="_method" value="DELETE">
+                        <button onclick="return confirm('Are you confirm to delete this blog')" class="btn btn-danger" type="submit">Delete</button>
+                    </form>
+                </td>
             </tr>
-<!--            --><?php //foreach ($blogs as $blog){ ?>
-            @foreach($blogs as $blog)
-                <tr>
-                    <td>{{ $blog->id }}</td>
-                    <td>{{ $blog->title }}</td>
-                    <td>{{ $blog->details }}</td>
-                    <td>
-                        <a class="btn btn-info" href="{{ route('blog.edit',$blog->id) }}">Edit</a>
-                        <form action="{{ route('blog.destroy',$blog->id) }}" method="post">
-                            @csrf
-                            <input type="hidden" name="_method" value="DELETE">
-                            <button onclick="return confirm('Are you confirm to delete this blog')" class="btn btn-danger" type="submit">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </table>
-
-    </div>
-</div>
-</body>
-</html>
+        @endforeach
+    </table>
+@endsection
