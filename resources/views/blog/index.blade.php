5d9abbb35ade4b2a1fc52e861d8aade15e737ca0
Shamimul Alam <bd.shawon.edu@gmail.com>
2019-05-24 11:05:22 +0600

    Templating Engine Blade Implemented

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title><?php echo $title; ?></title>
+    <title>{{ $title }}</title>
 </head>
 <body>
 <div class="row">
@@ -19,21 +19,22 @@
                 <th>Details</th>
                 <th>Actions</th>
             </tr>
-            <?php foreach ($blogs as $blog){ ?>
+<!--            --><?php //foreach ($blogs as $blog){ ?>
+            @foreach($blogs as $blog)
                 <tr>
-                    <td><?php echo $blog->id; ?></td>
-                    <td><?php echo $blog->title; ?></td>
-                    <td><?php echo $blog->details; ?></td>
+                    <td>{{ $blog->id }}</td>
+                    <td>{{ $blog->title }}</td>
+                    <td>{{ $blog->details }}</td>
                     <td>
-                        <a class="btn btn-info" href="<?php echo route('blog.edit',$blog->id); ?>">Edit</a>
-                        <form action="<?php echo route('blog.destroy',$blog->id); ?>" method="post">
-                            <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+                        <a class="btn btn-info" href="{{ route('blog.edit',$blog->id) }}">Edit</a>
+                        <form action="{{ route('blog.destroy',$blog->id) }}" method="post">
+                            @csrf
                             <input type="hidden" name="_method" value="DELETE">
                             <button onclick="return confirm('Are you confirm to delete this blog')" class="btn btn-danger" type="submit">Delete</button>
                         </form>
                     </td>
                 </tr>
-            <?php } ?>
+            @endforeach
         </table>
 
     </div>
